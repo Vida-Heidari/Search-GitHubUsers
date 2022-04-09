05563678f83cdb4d1b6ef23b3e497ead67b401eb
Vida Heidari <vidaaheidari@gmail.com>
2022-04-09 16:30:40 +0430

    snackbar styles added

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,6 @@
 const API_URL = "https://api.github.com/users/";
 
+const main = document.querySelector("#main");
 const searchBtn = document.querySelector(".search-btn");
 const inputSearch = document.querySelector("#username");
 const inputForm = document.querySelector(".form");
@@ -10,33 +11,66 @@ const userFollowers = document.querySelector(".followers");
 const userFollowing = document.querySelector(".following");
 const userLoginName = document.querySelector(".user__github__url");
 const userGithubAdrress = document.querySelector(".user__github__url");
+const repository = document.querySelector(".repository");
+
+const user = document.querySelector(".github-user");
+
+const hire = document.querySelector(".hire");
+
+const y = document.querySelector("#snackbar--empty-input");
+const x = document.querySelector("#snackbar--not-found");
+
+// --------------- FUNCTIONS ------------------
 
 const searchUser = (event) => {
-  // console.log({ input: inputSearch.value });
-
   let searchTerm = inputSearch.value;
 
   if (!searchTerm) {
-    alert("Search a GitHub Username");
+    // alert("Search a GitHub Username");
+
+    y.className = "show";
+    setTimeout(function () {
+      y.className = y.className.replace("show", "");
+    }, 3000);
     return;
   }
 
   // 2 req: Sync / Async
   fetch(API_URL + searchTerm, { method: "GET" })
     .then(function (response) {
+      // user.classList.remove("hidden");
+      if (!response.ok) {
+        const err = new Error(response.status);
+        throw err;
+      }
       return response.json();
     })
     .then((data) => {
+      user.classList.remove("hidden");
       render(data);
+      console.log(data);
+    })
+    .catch((error) => {
+      user.classList.add("hidden");
+      console.log(error);
+
+      x.className = "show";
+      setTimeout(function () {
+        x.className = x.className.replace("show", "");
+      }, 3000);
     });
 };
 
+// ------------------ EVENT LISTENERS ------------------
+
 searchBtn.addEventListener("click", searchUser);
+
 document.addEventListener("keydown", function (e) {
   console.log(e);
   if (e.key === "Enter") {
     searchUser();
   }
+  // hire.style.backgroundColor = "var(--green-color)";
 });
 
 function render(data) {
@@ -49,6 +83,7 @@ function render(data) {
     name,
     html_url,
     login,
+    public_repos,
   } = data;
   imgAvatar.src = avatar_url;
   userName.innerText = name;
@@ -56,4 +91,12 @@ function render(data) {
   userFollowing.innerText = following;
   userLoginName.innerText = login;
   userGithubAdrress.href = html_url;
+  // hire.innerText = hireable;
+  if (hireable) {
+    hire.classList.add("green");
+  } else {
+    hire.classList.remove("green");
+  }
+  // user.classList.remove("hidden");
+  repository.innerText = public_repos;
 }
